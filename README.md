e90cbd3f288db55094bbe670ef74949ea98d906c
LittleWhole <wholelittle@gmail.com>
2019-01-01 13:43:46 +0800

    Add LICENSE and README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Welcome to hi, the giveaway server
+This is the official website of hi, the giveaway server. You can feel free to contribute to this repository.
+
+This repository is licensed under the MIT License.
+
+This website is still in beta.
--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <html>
     <head>
         <title>hi - The Giveaway Server</title>
+        <link rel="icon" href="/imgs/hi.png">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <head>
     <body>
         <nav class="navbar navbar-default">
@@ -14,12 +16,15 @@
                     <a class="navbar-brand" href="#">hi - The Giveaway Server</a>
                 </div>
                     <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Giveaways</a></li>
-                        <li><a href="#">My Rewards</a></li>
-                        <li><a class="btn btn-dark" href="https://discord.gg/Xda3AVn" role="button">Join hi</a></li>
+                        <li class="nav-item active"><a href="#">Home</a></li>
+                        <li class="nav-item"><a href="/giveaways">Giveaways</a></li>
+                        <li class="nav-item"><a href="/account">My Rewards</a></li>
+                        <li class="nav-item"><a class="btn btn-light" href="https://discord.gg/Xda3AVn" role="button">Join hi</a></li>
                 </ul>
             </div>
         </nav>
     </body>
+    <footer>
+
+    </footer>
 </html>
