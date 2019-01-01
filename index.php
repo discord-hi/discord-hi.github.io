30a17ec25e7fdadf41300ca2842901181be53296
LittleWhole <wholelittle@gmail.com>
2019-01-01 12:49:53 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>hi - The Giveaway Server</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">hi - The Giveaway Server</a>
+                </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Giveaways</a></li>
+                        <li><a href="#">My Rewards</a></li>
+                        <li><a class="btn btn-dark" href="https://discord.gg/Xda3AVn" role="button">Join hi</a></li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
