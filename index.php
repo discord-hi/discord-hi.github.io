20996b1d47af31ec92599df7a5e0492b7806fe3e
LittleWhole <wholelittle@gmail.com>
2019-01-01 13:47:03 +0800

    Slight readjusting

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <title>hi - The Giveaway Server</title>
+        <link rel="icon" href="/imgs/hi.png">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">hi - The Giveaway Server</a>
+                </div>
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="/giveaways">Giveaways</a></li>
+                        <li><a href="/account">My Rewards</a></li>
+                        <li><a class="btn btn-light" href="https://discord.gg/Xda3AVn" role="button">Join hi</a></li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+    <footer>
+
+    </footer>
+</html>
