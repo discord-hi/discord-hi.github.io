22e8f66580dcd006b2a57770db23bd5fbdc317dd
MelonCola <puppyloganpuppy@gmail.com>
2019-02-03 02:05:09 -0500

    Fixed it(kinda)

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require("express"); //needed
-const session = require("express-session"); //needed I think
+const express = require("express");
+const session = require("express-session"); 
 const _ = require("lodash"); //???
 //const moment = require("moment"); //???
-const passport = require("passport"); //needed
-const DiscordStrategy = require("passport-discord").Strategy; //needed
-require("dotenv").config(); //useful so let's keep
+const passport = require("passport");
+const DiscordStrategy = require("passport-discord").Strategy;
+require("dotenv").config(); //put config in .env
 
 //const bot = require("./bot"); //probably going to run our bot here so ¯\_(ツ)_/¯
 //const db = require("./db"); //used for the databasing
 
-const app = express(); //??
+const app = express();
 const PORT = process.env.PORT || 8080;
 const URL = "http://hi.iceblaze.net";
 const scopes = ["identify", "guilds"];
-app.enable("trust proxy"); //???
-app.locals._ = _; //???
+app.enable("trust proxy"); 
+app.locals._ = _;
+app.use(express.static('views'));
+
 
 passport.serializeUser(function(user, done) {
   done(null, user);
@@ -56,15 +58,14 @@ app.get("/login", passport.authenticate("discord", { scope: scopes }),  (req, re
 app.get("/callback", passport.authenticate("discord", { failureRedirect: "/login" }), (req, res) => {
     res.redirect(req.session.returnTo || "/");
     delete req.session.returnTo;
-  }
-);
+});
 
-app.get("/", checkAuth, async (req, res) => {
-    res.sendFile("./views/index.html");
+app.get("/", (req, res) => {
+    res.sendFile("index.html");
 });
 
 app.get("/giveaways", (req, res) => {
-    res.sendFile("./views/giveaways.html");
+    res.sendFile("giveaways.html");
 });
 
 app.listen(PORT, () => {
